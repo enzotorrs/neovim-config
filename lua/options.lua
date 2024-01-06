b4ae0bff3cefece56077a40d5dd282a13df464d7
enzotorrs <enzotorres969@gmail.com>
2024-01-06 16:43:34 -0300

    add: general options file

---
+++ lua/options.lua
@@ -0,0 +1,18 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = ","
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.o.number = true
+vim.o.termguicolors = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.hidden = true
+vim.o.relativenumber = true
